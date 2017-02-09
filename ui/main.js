50c39a5f22a331f228a5962fc6b7a41fb6de2fd2
RaheelAbdulRashid <rsr9827@gmail.com>
2017-02-09 23:35:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 var element = document.getElementById("main_text");
 element.innerHTML="New text";
 
+var marginLeft=0;
+function moveRight() {
+    marginLeft= marginLeft+1;
+    img.style.marginLeft= marginLeft+"px";
+    
+}
 var img=document.getElementById("madi");
 img.onclick = function(){
-    img.style.marginLeft="100px";
+    var interval=setInterval(moveRight,50);
 };
