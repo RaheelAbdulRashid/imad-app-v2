ce9f1bc58b01200826ba1477a3e749dde6c96d4b
RaheelAbdulRashid <rsr9827@gmail.com>
2017-02-10 00:53:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,27 @@
-console.log('Loaded!');
-var element = document.getElementById("main_text");
-element.innerHTML="New text";
+var button=document.getElementById('counter');
+var counter=0;
 
-var marginLeft=0;
-function moveRight() {
-    marginLeft= marginLeft+1;
-    img.style.marginLeft= marginLeft+"px";
-    
-}
-var img=document.getElementById("madi");
-img.onclick = function(){
-    var interval=setInterval(moveRight,50);
+button.onclick=function(){
+  
+  
+  //making request object
+  var request= new XMLHttpsRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange=function()
+  {
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+        if(request,status===200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+            
+        }
+      }
+  };
+  //make a request to counter endpoint
+  request.open('GET','http://raheelabdulrashid.omad.hasura-app-io/counter',true);
+  request.send(null);
+  
 };
