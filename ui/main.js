05babe20b50e2318a29d148d070e69f43aaf034b
RaheelAbdulRashid <rsr9827@gmail.com>
2017-02-12 01:39:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick=function(){
       {
         if(request.status===200)
 */        {
-            var counter=request.responseText;
+            var counter=counter+1;
             var span=document.getElementById('count');
             span.innerHTML=counter.toString();
             
