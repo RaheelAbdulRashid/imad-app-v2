073b9245c2eb0c7a9127fcf7dac97160ea150b96
RaheelAbdulRashid <rsr9827@gmail.com>
2017-02-13 00:52:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick=function(){
         {
             var counter=counter+1;
             var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
+            span.innerHTML=counter;
             
         }
       }
