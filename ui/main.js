4c239eb90cf2032e30e950fc6c30ca58a84406b1
RaheelAbdulRashid <rsr9827@gmail.com>
2017-02-12 13:45:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var counter=0;
 
 button.onclick=function(){
 
-/*  
+  
     //making request object
   var request= new XMLHttpsRequest();
   //capture the response and store it in a variable
@@ -19,13 +19,11 @@ button.onclick=function(){
             
         }
       }
-  }
+  };
   //make a request to counter endpoint
   request.open('GET','http://raheelabdulrashid.imad.hasura-app.io/counter',true);
   request.send(null);
-*/
-var counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+
+
 
 };
