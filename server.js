31a5b93483afa659ba8149857753697327ce2f4a
RaheelAbdulRashid <rsr9827@gmail.com>
2017-02-12 15:45:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,9 +134,9 @@ app.get('/ui/main.js', function (req, res) {
 });
  
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){//https://---------/?name=xxxxx
   
-  var name=req.params.name;
+  var name=req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
 });
