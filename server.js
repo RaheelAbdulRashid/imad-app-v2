923bed2dd9a44719dbbab26cf119c5fd57e7161e
RaheelAbdulRashid <rsr9827@gmail.com>
2017-03-05 18:01:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.get('/submit-name',function(req,res){//https://---------/?name=xxxxx
 
 
 app.get('/articles/:articleName', function(req,res){
-   var articleName=req.params.articleName;
+   
    
    pool.query("select * from article where title='"+req.params.articleName+"'",function(err,result){
        if(err){
